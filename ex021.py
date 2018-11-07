64eacc210c638f4088fe4513709396f1bccd7ceb
LeroyLaw <wk.scbd@gmail.com>
2018-11-07 11:30:39 -0300

    Update: added lines of corrections.

--- ex021.py
+++ ex021.py
@@ -1,5 +1,7 @@
 import pygame
+pygame.mixer.init()
 pygame.init()
-pygame.mixer.music.load("k.mp3")
-pygame.mixer.music.play()
+pygame.mixer.music.load('D:/Law/Programação/Aulas/Exercicios/ex021.mp3')
+pygame.mixer.music.play
 pygame.event.wait()
+input('Deixando o programa aberto')
