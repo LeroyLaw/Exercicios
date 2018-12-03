059c30f66cdd2dd81e73e2339ab56d1bbfbeafa5
LeroyLaw <wk.scbd@gmail.com>
2018-12-03 15:08:34 -0300

    Added to Exercicios

---
+++ ex024.py
@@ -0,0 +1,2 @@
+c = input('Em que cidade você nasceu? ').lower().strip().split()
+print('santo' in c[0])
---
+++ ex027.py
@@ -0,0 +1,3 @@
+n = input('Digite seu nome completo: ').strip().title().split()
+print('Seu primeiro nome é {}!'.format(n[0]))
+print('Seu ultimo nome é {}!'.format(n[-1]))
